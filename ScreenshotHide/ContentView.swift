097f5ea6686edc6baebf59470e6b20e0c983e0c8
Arun Rathore <arathore@nowoptics.com>
2024-03-22 15:36:06 +0530

    Initial Commit

---
+++ ScreenshotHide/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ScreenshotHide
+//
+//  Created by Arun Rathore on 22/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ScreenshotHide/ScreenshotHideApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScreenshotHideApp.swift
+//  ScreenshotHide
+//
+//  Created by Arun Rathore on 22/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ScreenshotHideApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
